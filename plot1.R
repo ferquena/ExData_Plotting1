1847d422a4f038abd7a290b2c0598defd22dfd50
Maria Rodriguez <ferquena@gmail.com>
2015-03-07 18:29:27 +1100

    Updated file

--- plot1.R
+++ plot1.R
@@ -12,5 +12,5 @@ png("plot1.png", width=480, height=480, bg="transparent")
 hist(data.df$Global_active_power, main = "Global Active Power", xlab = "Global Active Power(kilowatts)", ylab = "Frequency" , col = "red")
 dev.off()
 
-on.exit(close(data))
+
 rm(list=ls())
